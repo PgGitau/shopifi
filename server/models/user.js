efbc3915fd595cb922a7ecc3df0fd3b65efc7614
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-11-18 21:35:44 +0300

    created a schema and model

---
+++ server/models/user.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true, //no white space before name
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true, //no white space before email
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true, //no white space before email
+      required: true,
+      min: 8,
+      max: 64,
+      // unique: true
+    },
+    address: {
+      type: String,
+      trim: true, //no white space before email
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
+  },
+  
+  {
+    timestamps: true,//give us created dated automatically
+  }
+); 
+
+export default mongoose.model("User", userSchema);
