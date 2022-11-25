a6823fda0f488b749b08c3dc6d866aa665bc8532
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-11-25 14:26:26 +0300

    add post route and its function(register function)

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,9 @@
-export const users = async (req, res) => {
-    res.json({
-        data: "John Robert Jones Kevin Peter from controller"
-    })
-}
+// export const users = async (req, res) => {
+//     res.json({
+//         data: "John Robert Jones Kevin Peter from controller"
+//     })
+// }
 
+export const register = async (req, res) => {
+    console.log(req.body);
+}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,8 +3,14 @@ import express from 'express';
 const router = express.Router();
 
 // controllers
-import {users} from '../controllers/auth.js';
+// import {users} from '../controllers/auth.js';
+import {register} from '../controllers/auth.js';
 
-router.get('/users', users);
+// testing GET route
+//  router.get('/users', users);
+
+
+// testing post route
+router.post('/register', register);
 
 export default router;
