8e659177b090a70d03e133f969ca36cd8d22ff4c
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-11-25 16:22:15 +0300

    add login function to check if password and email given is same as stored one

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -61,3 +61,49 @@ export const register = async (req, res) => {
         console.log(err)
     }
 }
+
+// for login to compare passwords
+export const login = async (req, res) => {
+    try {
+        // 1. destructure from req.body
+        const { email, password } = req.body;
+
+        // 2.all fields require validation
+        if (!email.trim()) {
+            return res.json({error: "Email is required"});
+        }
+        if (!password || password.length < 6) {
+            return res.json({error: "Password must be atleast 6 characters long"});
+        }
+
+        // 3. check if email exists
+        const user = await User.findOne({ email }); //same as {email: email}
+        if (!user) {
+            return res.json({error: "Email/User not found"})
+        }
+
+        // 4. compare password
+        const match = await comparePassword(password, user.password);
+        if (!match) {
+            return res.json({error: "Wrong password!!"})
+        }
+
+        // 5. create signed jwt to be used in protected routes
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+        // 6. send response
+        res.json({
+            user: {
+                name: user.name,
+                email: user.email,
+                address: user.address,
+                role: user.role,
+            },
+            token // same as token: token
+        });
+        
+        
+    } catch (err) {
+        console.log(err)
+    }
+}
