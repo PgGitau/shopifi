f69ec7618e8bd659d3266f09eb75e225d5b08e3d
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-11-25 18:37:35 +0300

    add jwtverify to verify user token to access protect route

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,4 +1,16 @@
+import jwt from 'jsonwebtoken';
+
 export const requireSignin = (req, res, next) => {
-    console.log('REQUEST HEADERS =>', req.headers);
-    next();
+    try {
+        //jwt.verify is used to verify the token with the jwt secret,
+        //if verified the token user can access the route,
+        //else he can't  
+        const decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+        // console.log("decoded =>", decoded)
+        req.user = decoded;
+        // console.log(req.user);
+        next();
+    } catch (err) {
+        return res.status(401).json(err);
+    };
 }
