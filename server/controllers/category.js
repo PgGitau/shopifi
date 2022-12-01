cc1eff1551171752f9747598753426f74197292b
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-12-01 21:14:42 +0300

    edit create function

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -5,12 +5,14 @@ import slugify from 'slugify';
 export const create = async (req, res) => {
     try {
         const { name } = req.body;
-        if(!name.trim) {
-            return res.json({error: "Name is required"});
+        if (!name.trim()) {
+            return res.json({error: "Name is required"})
         }
-        const existingCategory = await Category.findOne({ name });
+
+        // check if there's an existing category
+        const existingCategory = await Category.findOne({ name }); //same as {name: name}
         if (existingCategory) {
-            return res.json({error: "Already exists"});
+            return res.json({error: "Category already exists"})
         }
 
         const category = await new Category({ name, slug: slugify(name) });
