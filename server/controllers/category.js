9ac8249be75d337fdcdd3ebecb3508383b889e78
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-12-01 19:23:09 +0300

    add functions for CRUD functionality(update(U), read&list(R), remove(D))

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -21,3 +21,60 @@ export const create = async (req, res) => {
         return res.status(400).json(err);
     }
 };
+
+//update (first find cat thro' catId then update it)
+export const update = async (req, res) => {
+    try {
+        const { name } = req.body;
+        const { categoryId } = req.params;
+        const category = await Category.findByIdAndUpdate(
+            categoryId,
+            {
+                name,
+                slug: slugify(name)
+            },
+            {
+                new: true // used to save the update
+            }
+        );
+        res.json(category);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err.message);
+    }
+}
+
+//to delete (find cat thro' catId and delete it)
+export const remove = async (req, res) => {
+    try {
+        const { categoryId } = req.params;
+        const removed = await Category.findByIdAndDelete(categoryId);
+        res.json(removed);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err.message);
+    }
+}
+
+// to list categories (use .find to get all Cats)
+export const list = async (req, res) => {
+    try {
+        const all = await Category.find({});
+        res.json(all);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err.message);
+    }
+}
+
+// to list single category(based on slug that we'll receive
+// from router.get('/category/:slug', read);)
+export const read = async (req, res) => {
+    try {
+        const category = await Category.findOne({slug: req.params.slug}); 
+        res.json(category);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err.message);
+    }
+}
