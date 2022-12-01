02364f6b24c2edf4749c4faa38d5b98a813df8a7
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-12-01 19:22:10 +0300

    add CRUD(update(U), read&list(R), remove(D)) category routes to project

--- server/routes/category.js
+++ server/routes/category.js
@@ -6,9 +6,13 @@ const router = express.Router();
 import {requireSignin, isAdmin} from '../middlewares/auth.js';
 
 //controllers
-import { create } from "../controllers/category.js";
+import { create, update, remove, list, read } from "../controllers/category.js";
 
 // testing post route
 router.post('/category', requireSignin, isAdmin, create);
+router.put('/category/:categoryId', requireSignin, isAdmin, update); // to update category; we'll use catId to get specific cat to update
+router.delete('/category/:categoryId', requireSignin, isAdmin, remove); //to delete category; "    "    "
+router.get('/category', list); // to list all categories; no middlewares, publicly available
+router.get('/category/:slug', read); //to list a single category
 
 export default router;
