8d58fd0e26d2e0e918dbefdc27e7b59ac55e99c4
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-12-01 17:11:50 +0300

    add authentication for categories

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -4,7 +4,18 @@ import slugify from 'slugify';
 
 export const create = async (req, res) => {
     try {
-        console.log(req.body);
+        const { name } = req.body;
+        if(!name.trim) {
+            return res.json({error: "Name is required"});
+        }
+        const existingCategory = await Category.findOne({ name });
+        if (existingCategory) {
+            return res.json({error: "Already exists"});
+        }
+
+        const category = await new Category({ name, slug: slugify(name) });
+        category.save();
+        res.json(category);
     } catch (err) {
         console.log(err);
         return res.status(400).json(err);
