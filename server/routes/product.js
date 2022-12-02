f0b6737e3351b99c3ce96b5fdfeb82de86e5fb82
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-12-02 16:42:25 +0300

    add the rest CRUD ops routes(list(R), read(R), photo(R), remove(D), update(U))

--- server/routes/product.js
+++ server/routes/product.js
@@ -7,9 +7,15 @@ const router = express.Router();
 import {requireSignin, isAdmin} from '../middlewares/auth.js';
 
 //controllers
-import { create } from "../controllers/product.js";
+import { create, list, read, photo, remove, update } from "../controllers/product.js";
 
 // routes
 router.post('/product', requireSignin, isAdmin, formidable(), create);
+router.get('/products', list); // list all products
+router.get('/product/:slug', read); // get a single product
+router.get('/product/photo/:productId', photo); // get product photo
+router.delete('/product/:productId', requireSignin, isAdmin, remove); // remove a product 
+router.put('/product/:productId', requireSignin, isAdmin, formidable(), update); // update a product 
+
 
 export default router;
