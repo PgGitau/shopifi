c28c6db67e0664389c5888a295d7a296148f5f05
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-12-09 16:54:31 +0300

    add NavLink from router-dom to link to home,login,and register

---
+++ client/src/components/nav/Menu.js
@@ -0,0 +1,32 @@
+import { NavLink } from 'react-router-dom';
+
+function Menu() {
+    return (
+      <div className='nav-container border border-secondary mb-2'>
+        <ul className="nav d-flex justify-content-between m-1 p-2">
+
+          <li className="nav-item">
+            <NavLink className="nav-link" aria-current="page" to="/">
+              HOME
+            </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/login">
+              LOGIN
+            </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/register">
+              REGISTER
+            </NavLink>
+          </li>
+
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Menu;
+  
