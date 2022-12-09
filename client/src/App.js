5c5af211bd846971ea1f73ee89c7abfd4ad90fa3
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-12-09 16:55:07 +0300

    add Menu component to appear on top of all pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Menu from './components/nav/Menu';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -6,6 +7,9 @@ import Register from './pages/Register';
 function App() {
   return (
     <BrowserRouter>
+       {/* menu comp appears in all pages */}
+      <Menu /> 
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
