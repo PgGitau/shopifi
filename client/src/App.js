64f5ea5b47152d09c95b679bb2c0a252d4181a60
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-12-09 16:58:25 +0300

    small edits

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,8 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
