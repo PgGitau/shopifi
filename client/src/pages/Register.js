e80df6375f877173af0cd45eff78b9c63d28d612
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-12-21 22:00:29 +0300

    add address box to form, axios, toast for error messages when click submit button, and ToToastexxr

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,24 +1,40 @@
 import { useState } from 'react';
 import Jumbotron from '../components/cards/Jumbotron';
+import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
 
 function Register() {
     // state
-    const [ name, setName ] = useState("");
-    const [ email, setEmail ] = useState("");
-    const [ password, setPassword ] = useState("");
+    const [ name, setName ] = useState("Peter");
+    const [ email, setEmail ] = useState("peter@gmail.com");
+    const [ password, setPassword ] = useState("kilometer");
+    const [ address, setAddress ] = useState("123Nairobi");
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            console.log(name, email, password)
+            // destructure data from res.data console.log(res)
+            // to see what res contains after posting
+            const { data } = await axios.post(
+              `${process.env.REACT_APP_API}/register`,
+              { name, email, password, address }
+            );
+            console.log(data)
+            if (data?.error) {
+                toast.error(data.error);
+            } else {
+                toast.success("Registration successful")
+            }
         } catch (err) {
             console.log(err)
+            toast.error("Registration failed, try again!!")
         }
     }
 
     return (
       <div>
         <Jumbotron title="Register Page" subtitle="Please Register here" />
+        <Toaster />
 
         <div className="container mt-5 border border-warning p-2">
           <div className="row">
@@ -47,6 +63,14 @@ function Register() {
                   placeholder="Enter your Password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <input
+                  type="text"
+                  className="form-control mb-4 p-2"
+                  placeholder="Enter your Address"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
                 />
 
                 <button className="btn btn-primary" type="submit">
