4e24a74ffc74dcfabc4f219d71b7a04f8d947ab4
r3t3p <92801240+PgGitau@users.noreply.github.com>
2022-12-21 22:19:23 +0300

    add Toaster to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import Menu from './components/nav/Menu';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -9,7 +10,7 @@ function App() {
     <BrowserRouter>
        {/* menu comp appears in all pages */}
       <Menu /> 
-
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
