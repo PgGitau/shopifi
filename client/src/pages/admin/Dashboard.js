28c61d4c0049abec7f6fc8c7e1e996a1f0a911a6
r3t3p <92801240+PgGitau@users.noreply.github.com>
2023-01-13 16:24:59 +0300

    add admin dashboard content(menu on the left, content on right)

--- client/src/pages/admin/Dashboard.js
+++ client/src/pages/admin/Dashboard.js
@@ -1,4 +1,5 @@
 import Jumbotron from "../../components/cards/Jumbotron";
+import AdminMenu from "../../components/nav/AdminMenu";
 import { useAuth } from "../../context/auth";
 
 function AdminDashboard() {
@@ -11,7 +12,24 @@ function AdminDashboard() {
           title={`Hello ${auth?.user?.name}`}
           subtitle="Admin Dashboard"
         />
-        <pre>{JSON.stringify(auth, null, 4)}</pre>
+        {/* <pre>{JSON.stringify(auth, null, 4)}</pre> */}
+
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-3">
+              <AdminMenu />
+            </div>
+
+            <div className="col-md-9">
+              <div className="p-3 mt-2 mb-2 h4 bg-light">Admin Info</div>
+              <ul className="list-group">
+                <li className="list-group-item">{auth?.user?.name}</li>
+                <li className="list-group-item">{auth?.user?.email}</li>
+                <li className="list-group-item">You have Admin priviledges</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </>
     );
    
